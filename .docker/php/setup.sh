3f48b8c361c93b245a3ed9ad3de9092079c77a53
nt4rever <levantanald@gmail.com>
2023-08-10 21:14:55 +0700

    fix setup sh

--- .docker/php/setup.sh
+++ .docker/php/setup.sh
@@ -7,16 +7,16 @@ composer --no-interaction install
 
 cp .env.example .env
 
-php artisan telescope:install
-
 php artisan key:generate
 php artisan cache:clear
 php artisan config:clear
 php artisan route:clear
 php artisan view:clear
 
-# php artisan migrate
-# php artisan db:seed
+php artisan migrate
+php artisan db:seed
+
+php artisan telescope:install
 
 # npm install
 # npm run prod
